f86fc9c2c2da18e868a96a9aae8a6418c21552b9
Daniel Persson <persson.daniel.1990@gmail.com>
2020-11-08 23:46:13 +0100

    Add authors resolve to root query

--- types/RootQueryType.js
+++ types/RootQueryType.js
@@ -1,6 +1,7 @@
 import { GraphQLObjectType, GraphQLList } from "graphql";
 import { BookType } from "./BookType.js";
 import { authors, books } from "../data/fakeData.js"
+import { AuthorType } from "./AuthorType.js";
 
 export const RootQueryType = new GraphQLObjectType({
     name: "Query",
@@ -10,6 +11,11 @@ export const RootQueryType = new GraphQLObjectType({
             type: GraphQLList(BookType),
             description: "List of books",
             resolve: () => books
+        },
+        authors: {
+            type: new GraphQLList(AuthorType),
+            description: "List of all authors",
+            resolve: () => authors
         }
     })
 })
